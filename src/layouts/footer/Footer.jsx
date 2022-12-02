bb7e04c4a3a568465c7d0e00f7a16c1cdbcc0114
ChefDimas <dimalunik@aol.com>
2022-12-02 18:07:09 +0100

    Footer menu styling

--- src/layouts/footer/Footer.jsx
+++ src/layouts/footer/Footer.jsx
@@ -11,7 +11,55 @@ const Footer = () => {
                 </h1>
                 <button>Request Early Access</button>
             </div>
-            <div className="secondContainer"></div>
+            <div className={style.secondContainer}>
+                <ul>
+                    <li>
+                        Links
+                        <ul>
+                            <li>
+                                <a href="/">Overons</a>
+                            </li>
+                            <li>
+                                <a href="/">Social Media</a>
+                            </li>
+                            <li>
+                                <a href="/">Counters</a>
+                            </li>
+                            <li>
+                                <a href="/">Contact</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        Company
+                        <ul>
+                            <li>
+                                <a href="/">Terms & Conditions</a>
+                            </li>
+                            <li>
+                                <a href="/">Privacy Policy</a>
+                            </li>
+                            <li>
+                                <a href="/">Contact</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        Get in touch
+                        <ul>
+                            <li>
+                                <a href="">Crechterwoord K12 182 DK</a>
+                            </li>
+                            <li>
+                                <a href="">085-132567</a>
+                            </li>
+                            <li>
+                                <a href="">info@payme.net</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
         </footer>
     );
 };
